874026f665443fc3e5212e20dbc101d7f92ba898
shay876 <111309646+shay876@users.noreply.github.com>
2023-07-10 22:26:48 -0600

    add comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -6,9 +6,9 @@ function My_First_Function() {
     document.getElementById("Button_text").innerHTML = str2;
 }
 
-
+//This function uses the += operator
 function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this course";
-    document.getElementById("Concatenate").innerHTML = sentence;
+    var sentence = "I am learning";  //create variable
+    sentence += " a lot from this course"; //add += operator to add onto variable
+    document.getElementById("Concatenate").innerHTML = sentence; //use to change innerHTML of element with id concatenate
 }
