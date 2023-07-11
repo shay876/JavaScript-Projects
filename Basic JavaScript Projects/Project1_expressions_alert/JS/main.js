fe359a881bc6dbda80cf4b2a8dde3e10800c87a1
shay876 <111309646+shay876@users.noreply.github.com>
2023-07-10 21:32:46 -0600

    Start basic JS folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hey there!");
+
+document.write("Hi there!");
+
+//Create and display 2 variables
+var Sent1 = "Start of string";
+var Sent2 = "End of string";
+document.write(Sent1 + Sent2);
+
+
+//Expression
+document.write(6 + 7);
