19c9796ac9f6d20e2ae23c6a56bf55676fce1892
shay876 <111309646+shay876@users.noreply.github.com>
2023-07-11 21:33:06 -0600

    add project 3, complete addition and subtraction JS

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,18 @@
+
+//Create a function that returns the result of an addition operation 
+//to the paragraph element with the id “Math”
+
+function addition_Function() {
+
+    var addition = 6+4;
+    document.getElementById("Math").innerHTML = "6+4=" + addition;
+}
+
+//Create a function that returns the result of a subtraction operation 
+//to the paragraph element with the id “Math2”
+
+function subtraction_Function() {
+
+    var Subtraction = 6-4;
+    document.getElementById("Math2").innerHTML = "6-4=" + Subtraction;
+}
