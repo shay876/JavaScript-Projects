b59615b19a8583c7799752ad1014ef6f58f0016b
shay876 <111309646+shay876@users.noreply.github.com>
2023-07-16 20:45:21 -0600

    for loop

--- Basic JavaScript Projects/Project10_loops_arrays folder/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays folder/JS/main.js
@@ -8,3 +8,26 @@ function Call_Loop(){
     }
     document.getElementById("Loop").innerHTML = Digit;
 }
+
+
+//string length property
+
+function stringLength(){
+    var text1 = "Way to go, Captain America!";
+    document.getElementById("length").innerHTML= text1.length
+}
+
+
+//For Loop
+
+var Instruments=["Guitar","Drums","Piano","Bass","Violin","Trumpet","Flute"];
+
+var Content = "";
+var Y;
+
+function forLoop(){
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("listOfInstruments").innerHTML = Content;
+}
